54fa57c3bebff203bb6aa695796f23d85adbd52a
= <=>
2020-11-03 17:59:07 -0300

    Adicionando a pagina Index

---
+++ php-html/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta lang="pt-br">
+    <title>Tela Inicial</title>
+    <link rel="stylesheet" href="../css/global.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#" ><img src="../img/logo.svg" width="100" height="70" alt="Icone"></a>
+            <a class="ajuste" href="#Cadastre-se">Cadastre-se</a>
+            <a href="#Login">Login</a>
+        </nav>
+    </header>
+
+    <main>
+        <section class="painel-texto">
+            <div class="titulo">
+                <h1>&nbsp;Este e um tiulo de exemplo</h1>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; ● Topico 1</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; ● Topico 2</p>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp; ● Topico 3</p>
+            </div>
+            <div class="texto">
+                <img src="../img/img1.svg" alt="img1" width="100" height="70" >
+                Titulo 1
+                <br><br>
+                Exemplo de texto sobre um subtema.
+            </div>
+
+            <div class="texto">
+                <img src="../img/img2.svg" alt="img2" width="100" height="70" >
+                Titulo 2
+                <br><br>
+                Exemplo de texto sobre um subtema.
+            </div>
+
+            <div class="texto">
+                <img src="../img/img3.svg" alt="img3" width="100" height="70" >
+                Titulo 3
+                <br><br>
+                Exemplo de texto sobre um subtema.
+            </div>
+            <div class="texto">
+                <img src="../img/img4.svg" alt="img4" width="100" height="70" >
+                Titulo 4
+                <br><br>
+                Exemplo de texto sobre um subtema.
+            </div>
+
+        </section>
+        <section class="painel-imagem">
+            <img src="../img/img titulo.svg" alt="">
+        </section>
+    </main>
+    
+    <footer>
+        <p class="rodape">®Recode Pro 2020 - Todos os direitos reservados</p>
+    </footer>
+</body>
+</html>
